c44ada97f1e024eb1ced860ecd52b989f1ff917a
Sadman <sadmanh23@gmail.com>
2018-07-15 16:25:43 +0600

    Add initial files

---
+++ main.js
@@ -0,0 +1,47 @@
+// var password = '';
+var symbols = ['!', '@', '$', '&', '#', '?'];
+var words = '';
+
+// Get JSON word list
+var request = new XMLHttpRequest();
+request.open('GET', 'https://raw.githubusercontent.com/dariusk/corpora/master/data/words/common.json', true);
+request.onload = function() {
+	if (request.status >= 200 && request.status < 400) {
+		data = JSON.parse(request.responseText);
+		console.log("Data successfully fetched.");
+	} else {
+		console.log('Error fetching data.');
+	}
+}
+request.onerror = function() {
+	console.log('Error fetching data.');
+}
+request.send();
+
+function createString(data) {
+	var numbers = Math.floor(Math.random() * 99);
+	var symbol = symbols[Math.floor(Math.random() * symbols.length)];
+	console.log(symbol);
+	console.log(numbers);
+	var password = '';
+	while(password.length <= 16) {
+		word = data[Math.floor(Math.random() * data.length)];
+		if (word.length > 3 && word.length < 8) {
+			password += word;
+			words += word + ' ';
+		}
+	}
+	password += numbers;
+	let retArray = [];
+	retArray.push(password);
+	retArray.push(words);
+	return retArray;
+}
+
+function generatePassword() {
+	var password = createString(data.commonWords)[0];
+	var words = createString(data.commonWords)[1];
+	document.getElementById('password-field').value = password;
+	console.log(`Password is: ${password}`);
+	console.log(words);
+}
