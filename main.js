08a2c121e28a983796d03fcf3eb3ecaec1000be6
Sadman <sadmanh23@gmail.com>
2018-07-15 16:37:29 +0600

    Add uppercase letter

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// var password = '';
-var symbols = ['!', '@', '$', '&', '#', '?'];
-var words = '';
-
 // Get JSON word list
 var request = new XMLHttpRequest();
 request.open('GET', 'https://raw.githubusercontent.com/dariusk/corpora/master/data/words/common.json', true);
@@ -20,9 +16,9 @@ request.send();
 
 function createString(data) {
 	var numbers = Math.floor(Math.random() * 99);
+	var symbols = ['!', '@', '$', '&', '#', '?'];
 	var symbol = symbols[Math.floor(Math.random() * symbols.length)];
-	console.log(symbol);
-	console.log(numbers);
+	var words = '';
 	var password = '';
 	while(password.length <= 16) {
 		word = data[Math.floor(Math.random() * data.length)];
@@ -31,7 +27,9 @@ function createString(data) {
 			words += word + ' ';
 		}
 	}
+	password = password.charAt(0).toUpperCase() + password.substr(1);
 	password += numbers;
+	words += numbers;
 	let retArray = [];
 	retArray.push(password);
 	retArray.push(words);
@@ -39,8 +37,9 @@ function createString(data) {
 }
 
 function generatePassword() {
-	var password = createString(data.commonWords)[0];
-	var words = createString(data.commonWords)[1];
+	var retArray = createString(data.commonWords);
+	var password = retArray[0];
+	var words = retArray[1];
 	document.getElementById('password-field').value = password;
 	console.log(`Password is: ${password}`);
 	console.log(words);
