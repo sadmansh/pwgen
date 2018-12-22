d4cc107654c0712acc17e7a0b9d21c5c317d6af5
Sadman <sadmanh23@gmail.com>
2018-12-22 23:27:58 +0600

    Show static variable in password field

--- src/components/GeneratePassword.js
+++ src/components/GeneratePassword.js
@@ -3,21 +3,27 @@ import React, { Component } from 'react';
 class GeneratePassword extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { passGenerated: true };
+		this.state = { 
+			passGenerated: true,
+			password: ''
+		};
 
 		this.generate = this.generate.bind(this);
 	}
 
 	generate() {
+		let newPass = 'Skylinesplitsin2!';
+
 		this.setState(state => ({
-			passGenerated: !state.passGenerated
+			passGenerated: false,
+			password: newPass
 		}));
 	}
 
 	render() {
 		return (
-			<div ClassName="Generator">
-				<input type="text" placeholder="Your password..." />
+			<div className="Generator">
+				<input type="text" placeholder="Your password..." defaultValue={this.state.password}/>
 				<button onClick={this.generate}>{this.state.passGenerated ? 'Generate Password' : 'Get Another'}</button>
 			</div>
 		)
