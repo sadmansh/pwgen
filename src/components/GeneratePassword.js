eaccb87963463f4b7fda4689385e6b10974ea1f6
Sadman <sadmanh23@gmail.com>
2018-12-22 23:31:01 +0600

    MVP ready

--- src/components/GeneratePassword.js
+++ src/components/GeneratePassword.js
@@ -12,7 +12,9 @@ class GeneratePassword extends Component {
 	}
 
 	generate() {
-		let newPass = 'Skylinesplitsin2!';
+		// let newPass = 'Skylinesplitsin2!';
+		let retArray = createString(data.commonWords);
+		let newPass = retArray[0];
 
 		this.setState(state => ({
 			passGenerated: false,
@@ -28,6 +30,45 @@ class GeneratePassword extends Component {
 			</div>
 		)
 	}
+}
+
+let request = new XMLHttpRequest();
+let data = {};
+request.open('GET', 'https://raw.githubusercontent.com/dariusk/corpora/master/data/words/common.json', true);
+request.onload = function() {
+	if (request.status >= 200 && request.status < 400) {
+		data = JSON.parse(request.responseText);
+		// console.log("Data successfully fetched.");
+	} else {
+		console.error('Error fetching data.');
+	};
+}
+request.onerror = function() {
+	console.error('Error fetching data.');
+}
+request.send();
+
+function createString(data) {
+	let numbers = Math.floor(Math.random() * 99);
+	let symbols = ['!', '@', '$', '&', '#', '?'];
+	let symbol = symbols[Math.floor(Math.random() * symbols.length)];
+	let words = '';
+	let word = '';
+	let password = '';
+	while(password.length <= 16) {
+		word = data[Math.floor(Math.random() * data.length)];
+		if (word.length > 3 && word.length < 8) {
+			password += word;
+			words += word + ' ';
+		}
+	}
+	password = password.charAt(0).toUpperCase() + password.substr(1);
+	password += numbers + symbol;
+	words += numbers + ' ' + symbol;
+	let retArray = [];
+	retArray.push(password);
+	retArray.push(words);
+	return retArray;
 }
 
 export default GeneratePassword;
